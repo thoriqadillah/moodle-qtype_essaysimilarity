c21b9fce2346d36064bea6c03075f455deebbd36
thoriqadillah <thoriqadillah59@gmail.com>
2022-12-27 13:03:51 +0700

    add explanation

--- nlp/preprocessing/transformer/lsa.php
+++ nlp/preprocessing/transformer/lsa.php
@@ -18,6 +18,12 @@ class lsa {
     $svd = new svd($matrix);
     $min = min($features ?? $svd::$K, $m, $n);
 
+    // not sure what to use, U or V
+    // U based on https://stackoverflow.com/a/1039035/19323874
+    // V based on https://www.youtube.com/watch?v=K38wVcdNuFc&t=199s
+    // 
+    // In theory, the U is for word to concept, and V is document to concept
+    // So, I think it is more appropriate to use U like code below
     $matrix->truncate($svd::$U, $m, $min);
     $Ut = $matrix->transpose($svd::$U);
 
